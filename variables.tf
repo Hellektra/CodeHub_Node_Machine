518b3cc6d50e2a645701d94fdb711a3b4f54a7d3
Hellektra <32981306+Hellektra@users.noreply.github.com>
2023-04-22 22:00:37 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "location" {}
 variable "admin_username" {}
 variable "admin_password" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
