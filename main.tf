22920dbc714e2afb1156fc27410fc3e3a4d0f0f0
Hellektra <32981306+Hellektra@users.noreply.github.com>
2023-04-22 21:25:59 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_network" "vnet"{
 
 #Create a subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "project-codehub-subnet"
+  name                 = "node-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name          #The subnet is part of the virtual network created above
   address_prefixes     = ["10.0.2.0/24"]
