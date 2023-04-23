b444ef7e1228aae3de2ede00ca3a5cab7e0155e0
Hellektra <32981306+Hellektra@users.noreply.github.com>
2023-04-23 13:32:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "azurerm_network_security_group" "nsg_node_machine" {
 
 # Create network interface
 resource "azurerm_network_interface" "netif" {
-  name                = "project-codehub-nic"
+  name                = "project-codehub-nic_node"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
